a8b9391a956119300a60f52b22caf5baa4304904
zainab-hashmii <zainabhashmi1427@gmail.com>
2025-07-30 13:45:11 -0700

    Added components and jobs.json

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Card from './Card';
+
 
 const HomeCards = () => {
   return (
@@ -6,7 +8,7 @@ const HomeCards = () => {
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+          <Card>
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
@@ -17,9 +19,10 @@ const HomeCards = () => {
             >
               Browse Jobs
             </a>
-          </div>
-          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Employers</h2>
+          </Card>
+
+          <Card bg='bg-indigo-100'>
+             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
@@ -29,9 +32,12 @@ const HomeCards = () => {
             >
               Add Job
             </a>
-          </div>
+          </Card>
+           
+            
         </div>
       </div>
+      
     </section>
   )
 }
